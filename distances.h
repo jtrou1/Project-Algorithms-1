11ec4700c786c46fa8ba670621861ead3e19feb4
Hepic <hepic135@gmail.com>
2017-12-24 14:19:05 +0200

    Add distances

---
+++ distances.h
@@ -0,0 +1,20 @@
+#ifndef DISTANCES_H
+#define DISTANCES_H
+
+#include <iostream>
+#include <vector>
+#include "curve.h"
+
+using namespace std;
+
+static Curve default_curve;
+extern double **mem_distance;
+
+void find_closest_point(vector<double>&, const vector<double>&, const vector<double>&, double);
+double euclidean_distance(const vector<double>&, const vector<double>&);
+void mean_point(vector<double> &mean, const vector<double> &pnt_1, const vector<double> &pnt_2);
+double discrete_frechet_distance(const Curve&, const Curve&, Curve& = default_curve, bool = false);
+double dynamic_time_wrapping(const Curve&, const Curve&);
+double compute_distance(const Curve &curve_1, const Curve &curve_2, const char*);
+
+#endif
