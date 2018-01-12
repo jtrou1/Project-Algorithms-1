5c4b1539cf910380f7d6c6b1854535d1e2a7a6d4
Hepic <hepic135@gmail.com>
2018-01-12 20:24:46 +0200

    Cut roads dependent on curvature and junction

--- file_functions.cpp
+++ file_functions.cpp
@@ -2,10 +2,13 @@
 #include <fstream>
 #include <vector>
 #include <cstring>
+#include <map>
 #include "file_functions.h"
 #include "help_functions.h"
 
 using namespace std;
+
+typedef pair<double, double> PDD;
 
 void read_file(const char *file_name, const char *type) {
     ifstream file(file_name);
@@ -13,13 +16,13 @@ void read_file(const char *file_name, const char *type) {
     int num_conform, num_points, dim;
     int way_id, int_id = 0;
     char chr;
-    
+
     if (!strcmp(type, "1")) { // particles
         dim = 3;
         point.resize(dim);
-        
+
         file >> num_conform >> num_points;
-        
+
         for (int i = 1; i <= num_conform; ++i) {
             Curve curve("", int_id++, dim);
 
@@ -34,7 +37,7 @@ void read_file(const char *file_name, const char *type) {
 
                 curve.insert_point(point);
             }
-            
+
             input_curves.push_back(curve);
         }
     } else if (!strcmp(type, "2")) { // roads
@@ -48,7 +51,7 @@ void read_file(const char *file_name, const char *type) {
             for (int i = 1; i <= num_points; ++i) {
                 for (int j = 0; j < dim; ++j) {
                     file >> point[j];
-                    
+
                     if (i < num_points || j < dim - 1) {
                         file >> chr;
                     }
@@ -60,7 +63,7 @@ void read_file(const char *file_name, const char *type) {
 
                 curve.insert_point(point);
             }
-            
+
             input_curves.push_back(curve);
         }
     }
@@ -70,49 +73,28 @@ void read_file(const char *file_name, const char *type) {
 
 void print_segments(const char *init_csv, const char *final_csv) {
     ifstream init_file(init_csv);
-    ofstream final_file(final_csv); 
+    ofstream final_file(final_csv);
     int way_id, int_id = 0, dim = 2;
-    double circ_rad;
+    double curvature;
     char chr;
     vector<double> point(dim);
-     
+    map<PDD, int> mem_points;
+
     while (init_file >> way_id) {
         init_file >> chr;
         while (init_file >> chr && chr >= 'a' && chr <= 'z') {} // read 'road type' and 'comma'
 
-        Curve curve;
-        circ_rad = 0;
-        
         while (1) {
-            for (int j = 0; j < dim; ++j) {
-                init_file >> point[j] >> chr;
+            for (int i = 0; i < dim; ++i) {
+                init_file >> point[i] >> chr;
             }
-            
-            if (curve.get_length() >= 2) {
-                double x1 = curve.get_coord_point(0, curve.get_length() - 2);
-                double y1 = curve.get_coord_point(1, curve.get_length() - 2);
-                double x2 = curve.get_coord_point(0, curve.get_length() - 1);
-                double y2 = curve.get_coord_point(1, curve.get_length() - 1);
-                double x3 = point[0];
-                double y3 = point[1];    
-                
-                circ_rad = circumradius(x1, y1, x2, y2, x3, y3);
-            }
-            
-            cout << circ_rad << endl;
-            if (circ_rad > 4) {
-                final_file << int_id++ << "," << way_id << ",";
-                print_curve(final_file, curve, dim);
-                
-                circ_rad = 0;
-                curve.clear_curve();
-                curve.insert_point(point);
+
+            if (!mem_points.count(make_pair(point[0], point[1]))) {
+                mem_points[make_pair(point[0], point[1])] = 1;
             } else {
-                if (curve.is_empty() || curve.get_last_point() != point) { // remove duplicates
-                    curve.insert_point(point);
-                }
+                ++mem_points[make_pair(point[0], point[1])];
             }
-            
+
             if (init_file.eof()) {
                 break;
             }
@@ -122,9 +104,78 @@ void print_segments(const char *init_csv, const char *final_csv) {
                 break;
             }
         }
-        
-        final_file << int_id++ << "," << way_id << ",";
-        print_curve(final_file, curve, dim);
+    }
+
+    init_file.clear();
+    init_file.seekg(0, init_file.beg);
+
+    while (init_file >> way_id) {
+        init_file >> chr;
+        while (init_file >> chr && chr >= 'a' && chr <= 'z') {} // read 'road type' and 'comma'
+
+        Curve curve;
+        curvature = 0;
+
+        while (1) {
+            for (int i = 0; i < dim; ++i) {
+                init_file >> point[i] >> chr;
+            }
+
+            if (curve.get_length() >= 3) {
+                double x0 = curve.get_coord_point(0, curve.get_length() - 3);
+                double y0 = curve.get_coord_point(1, curve.get_length() - 3);
+                double x1 = curve.get_coord_point(0, curve.get_length() - 2);
+                double y1 = curve.get_coord_point(1, curve.get_length() - 2);
+                double x2 = curve.get_coord_point(0, curve.get_length() - 1);
+                double y2 = curve.get_coord_point(1, curve.get_length() - 1);
+                double x3 = point[0];
+                double y3 = point[1];
+
+                double circ_rad_1 = circumradius(x0, y0, x1, y1, x2, y2);
+                double circ_rad_2 = circumradius(x1, y1, x2, y2, x3, y3);
+
+                curvature = min(circ_rad_1, circ_rad_2);
+            }
+
+            if (curve.is_empty() || curve.get_last_point() != point) { // remove duplicates
+                if (curvature > 0.05) {
+                    final_file << int_id++ << "," << way_id << ",";
+
+                    vector<double> keep_pnt = curve.get_last_point();
+                    curve.remove_last_point();
+
+                    print_curve(final_file, curve, dim);
+
+                    curvature = 0;
+                    curve.clear_curve();
+
+                    curve.insert_point(keep_pnt);
+                } else if (mem_points[make_pair(point[0], point[1])] >= 2 && curve.get_length() > 10) {
+                    final_file << int_id++ << "," << way_id << ",";
+                    print_curve(final_file, curve, dim);
+
+                    curvature = 0;
+                    curve.clear_curve();
+                    --mem_points[make_pair(point[0], point[1])];
+                }
+
+                curve.insert_point(point);
+            }
+
+            if (init_file.eof()) {
+                break;
+            }
+
+            if (chr != ',') {
+                init_file.seekg(-1, init_file.cur);
+                break;
+            }
+        }
+
+        if (curve.get_length() >= 2) {
+            final_file << int_id++ << "," << way_id << ",";
+            print_curve(final_file, curve, dim);
+        }
     }
 
     init_file.close();
@@ -143,7 +194,7 @@ void print_file(const char *file_name, const vector<vector<int> > &clusters, dou
 
     file << "k: " << non_empty_clusters << "\n";
     file << "s: " << silhouette_value << "\n";
-    
+
     for (int i = 0; i < (int)clusters.size(); ++i) {
         if (clusters[i].empty()) {
             continue;
@@ -152,7 +203,7 @@ void print_file(const char *file_name, const vector<vector<int> > &clusters, dou
         for (int j = 0; j < (int)clusters[i].size() - 1; ++j) {
             file << clusters[i][j] << " ";
         }
-            
+
         file << clusters[i][(int)clusters[i].size() - 1] << "\n";
     }
 
